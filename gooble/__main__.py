2a39fda36f74e97898d1553d4045282e5d0cae7c
Chris Blackburn <chrisnblackburn@gmail.com>
2022-02-24 02:54:07 -0500

    redundant

--- gooble/__main__.py
+++ gooble/__main__.py
@@ -8,12 +8,11 @@ from . import Gooble
 from .logs import getLogger
 logger = getLogger()
 
-if __name__=="__main__":
-    logger.info("Welcome to gooble")
-    token = os.getenv("TOKEN")
-    if not token:
-        logger.error("Please set the TOKEN environment variable")
-        sys.exit(1)
+logger.info("Welcome to gooble")
+token = os.getenv("TOKEN")
+if not token:
+    logger.error("Please set the TOKEN environment variable")
+    sys.exit(1)
 
-    gooble = Gooble()
-    gooble.run(token)
+gooble = Gooble()
+gooble.run(token)
